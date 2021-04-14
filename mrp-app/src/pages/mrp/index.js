fe6149a7a80222de2e0fa7d8824c776bc1ab3aaa
DanielVargas13 <daniel.vargas@sgapucminas.br>
2021-04-14 19:00:52 -0300

    Embelezando o título da página de MRP

--- mrp-app/src/pages/mrp/index.js
+++ mrp-app/src/pages/mrp/index.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, Typography }  from '@material-ui/core';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -8,23 +8,16 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-/*function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-  }
-  
-  const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];*/
-
-  const useStyles = makeStyles({
+  const useStyles = makeStyles((theme) => ({
     table: {
       width: '90%',
     },
-  });
+    title:{
+        textAlign: 'center',
+        margin: theme.spacing(3),
+        fontFamily: 'Questrial, sans-serif '
+    },
+  }));
 
   /*function TesteComponentes(){
       var array=[
@@ -637,6 +630,7 @@ import Paper from '@material-ui/core/Paper';
   }
 
 function MRP({history}){
+    const classes = useStyles();
     var componentes = history.location.state;
     var numeroPeriodos = componentes[0].NumeroPeriodos;
     
@@ -647,7 +641,9 @@ function MRP({history}){
 
     return(
         <Fragment>
-            <h1>MRPs Gerados</h1>
+            <Typography variant="h4" gutterBottom className={classes.title}>
+                MRPs Gerados
+            </Typography>
             {ConstruirTabela(componentes)}
     </Fragment>
     );
